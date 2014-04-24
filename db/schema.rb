5e3d4ea627a553e9a87eaaa3e3e8031af090c754
App Academy Student <student@appacademy.io>
2014-04-24 17:05:50 -0400

    adds tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140424125413) do
+ActiveRecord::Schema.define(version: 20140424205922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,14 @@ ActiveRecord::Schema.define(version: 20140424125413) do
 
   add_index "notifications", ["notifier_id"], name: "index_notifications_on_notifier_id", using: :btree
   add_index "notifications", ["user_id"], name: "index_notifications_on_user_id", using: :btree
+
+  create_table "tags", force: true do |t|
+    t.string   "tag"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username",            null: false
