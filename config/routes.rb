8539fe172f7571227fecf8e480d706ffe9f3c78d
App Academy Student <student@appacademy.io>
2014-04-24 13:57:40 -0400

    tidies up routes a bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 DArt::Application.routes.draw do
   root to: "deviations#index"
 
+  resource :session, only: [:new, :create, :destroy]
+
   resources :deviations
 
   shallow do
     resources :users do
-      resources :journals
       resources :galleries
-      resources :messages
+      resources :journals
+      resources :messages, except: [:edit, :update]
+      resources :notifications, only: [:index]
     end
-  end
-
-  resources :users do
-    resources :notifications, only: :index
   end
 
   resources :likes, only: [:create, :destroy]
   resources :comments, only: [:create, :destroy]
-  resource :session, only: [:new, :create, :destroy]
 end
